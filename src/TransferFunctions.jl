200734dc1e6b6489c40341296620b7115823a96b
Adebesin <ldev515@pc58585al.retail2u.trcg.co.uk>
2018-11-25 17:00:42 +0000

    activation functions

--- src/TransferFunctions.jl
+++ src/TransferFunctions.jl
@@ -3,25 +3,26 @@
 =#
 
 import Base.MathConstants.e
+import Plots
 
-function hardLimit(n::Number)::Int
+function hardLimit(n::Number)::Number
     if n >= 0 1
     else 0
     end
 end
 
-function symetricalHardLimit(n::Number)::Int
+function symetricalHardLimit(n::Number)::Number
     if n >= 0 1
     else -1
     end
 end
 
-function linear(n::Number)::Int
+function linear(n::Number)::Number
     n
 end
 
 function saturatingLinear(n::Number)::Number
-    if (n >= 0) & (n <= 1) n
+    if (n >= 0) && (n <= 1) n
     elseif n < 0 0
     else 1
     end
@@ -41,3 +42,16 @@ end
 function hyperbolicTangentSigmoid(n::Number)::Float64
     (e^n - e^-n) / (e^n + e^-n)
 end
+
+function positiveLinear(n::Number)::Number
+    if 0 <= n n
+    else 0
+    end
+end
+
+# Max number in culumn == 1 all others in column 0
+function competetive(n::Array)::Array
+    n
+end
+
+competetive([0; 1; -0.5; 0.5])
